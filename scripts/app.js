046f23f0782062325294a849c56f04de01c3cf39
Aleksandra Nowak <122985468+anowak7@users.noreply.github.com>
2023-01-26 15:03:34 +0100

    Added API and corrected changing of units

--- scripts/app.js
+++ scripts/app.js
@@ -3,26 +3,30 @@ const productsEl = document.querySelector(".products");
 const cartItemsEl = document.querySelector(".cart-items");
 const subtotalEl = document.querySelector(".subtotal");
 
+let products = [];
+
 function renderProducts() {
   products.forEach((product) => {
     productsEl.innerHTML += `
-    <div class="item">
+    <div class="item" id="${product.id}">
     <div class="item-container">
+        <div class="item-img-desc">
         <div class="item-img">
-            <img src="${product.imgSrc}" alt="${product.name}">
+        <img src="${product.images[0]}" alt="${product.title}">
         </div>
         <div class="desc">
-            <h2>${product.name}</h2>
-            <h3>${product.manufacturer}</h3>
-            <p>
-            ${product.description}
-            </p>
+        <h2>${product.title}</h2>
+        <h3>${product.brand}</h3>
+        <p>
+        ${product.description}
+        </p>
         </div>
+      </div>
         <div class="add-to-cart" >
           <h2><small>$</small>${product.price}</h2>
-          <div class="btn minus">-</div>
-          <div class="product-amount">1</div>
-          <div class="btn plus">+</div>
+          <div class="btn minus" onClick="handleQuantityChange(${product.id}, -1)">-</div>
+          <div class="product-unit">1</div>
+          <div class="btn plus" onClick="handleQuantityChange(${product.id}, 1)">+</div>
           <img src="./assets/add-to-cart.png" alt="add to cart" onClick="addToCart(${product.id})">
         </div>
     </div>
@@ -30,22 +34,49 @@ function renderProducts() {
     `;
   });
 }
-renderProducts();
+
+function getItemQuantityElement(id) {
+  return document.getElementById(`${id}`).querySelector(".product-unit");
+}
+
+function handleQuantityChange(id, qty) {
+  const currentQtyElement = getItemQuantityElement(id);
+  const currentQty = Number(currentQtyElement.innerText);
+
+  const newQty = Math.max(currentQty + qty, 1);
+
+  currentQtyElement.innerText = newQty;
+}
 
 //cart array
 let cart = JSON.parse(localStorage.getItem("CART")) || [];
 updateCart();
 
+//sorting by manufacturer
+// function sortingByManufacturer() {
+//   cart.sort(function (a, b) {
+//     if (a.manufacturer.toLowerCase() < b.manufacturer.toLowerCase()) return -1;
+//     if (a.manufacturer.toLowerCase() > b.manufacturer.toLowerCase()) return 1;
+//     return 0;
+//   });
+//
+// }
+
 //ADD to cart
 function addToCart(id) {
+  const qtyElement = getItemQuantityElement(id);
+
+  const unitsToBeAdded = Number(qtyElement.innerText);
+  qtyElement.innerText = 1; // after adding to cart, reset counter
+
   //check if product already exists in cart
   if (cart.some((item) => item.id === id)) {
-    changeNumberOfUnits("plus", id);
+    changeNumberOfUnits(unitsToBeAdded, id);
   } else {
     const item = products.find((product) => product.id === id);
     cart.push({
       ...item,
-      numberOfUnits: 1,
+      numberOfUnits: unitsToBeAdded,
     });
   }
 
@@ -80,15 +111,15 @@ function renderCartItems() {
     cartItemsEl.innerHTML += `
     <div class="cart-item">
                     <div class="item-info">
-                        <h4>${item.name}</h4>
+                        <h4>${item.title}</h4>
                     </div>
                     <div class="unit-price">
                         <small>$</small>${item.price}
                     </div>
                     <div class="units">
-                        <div class="btn minus" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+                        <div class="btn minus" onclick="changeNumberOfUnits(-1, ${item.id})">-</div>
                         <div class="number">${item.numberOfUnits}</div>
-                        <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>
+                        <div class="btn plus" onclick="changeNumberOfUnits(1, ${item.id})">+</div>
                     </div>
                     <div class="btn-delete" onclick="removeItemFromCart(${item.id})">
                       <img src="./assets/delete.png" alt="delete button">
@@ -104,16 +135,12 @@ function removeItemFromCart(id) {
   updateCart();
 }
 
-//change number of units for an item
+//change number of units for an item in the cart
 function changeNumberOfUnits(action, id) {
   cart = cart.map((item) => {
     let numberOfUnits = item.numberOfUnits;
     if (item.id === id) {
-      if (action === "minus" && numberOfUnits > 1) {
-        numberOfUnits--;
-      } else if (action === "plus" && numberOfUnits < item.instock) {
-        numberOfUnits++;
-      }
+      numberOfUnits = Math.max(1, numberOfUnits + action);
     }
     return {
       ...item,
@@ -122,3 +149,12 @@ function changeNumberOfUnits(action, id) {
   });
   updateCart();
 }
+
+async function fetchProducts() {
+  const response = await fetch("https://dummyjson.com/products");
+  const json = await response.json();
+  products = json.products.map((p) => ({ ...p, numberOfUnits: 0 }));
+  renderProducts();
+}
+
+fetchProducts();
